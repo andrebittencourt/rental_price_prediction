2ab1a0991ca1d73cd29510347e36556f7baf7fdc
André Bittencourt <andrecb@gmail.com>
2021-07-21 02:26:30 -0300

    completed cleansing

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,10 @@ Downloads raw dataset from W&B, applies basic cleaninsing and exports to a new a
 import argparse
 import logging
 import wandb
+import pandas as pd
+import tempfile
+from wandb_utils.log_artifact import log_artifact
+import os
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -20,9 +24,32 @@ def go(args):
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info(f"Fetching artifact {args.input_artifact}")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    logger.info("Artifact retrieved, reading it locally.")
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("Data read. Starting cleansing.")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info(f"Data cleansed. Saving and uploading {args.output_artifact} artifact.")
+    with open(args.output_artifact, "w") as fp:
+        df.to_csv(fp.name, index=False)
+        log_artifact(
+            args.output_artifact,
+            args.output_type,
+            args.output_description,
+            fp.name,
+            run
+        )
+    
+    logger.info("Cleaned data artifact registered. Tidying up temp files.")
+    os.remove(args.output_artifact) #tidying up
+    logger.info("DONE - CLEANING")
 
 
 if __name__ == "__main__":
@@ -32,43 +59,43 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type = str,
+        help= "Raw data artifact to be cleaned",
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type= str,
+        help= "Name of cleaned data artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type= str,
+        help= "Type of output artifact",
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type= str,
+        help= "Description of output artifact",
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type= float,
+        help= "Min allowed price for outliear removal",
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type= float,
+        help= "Max allowed price for outliear removal",
         required=True
     )
 
